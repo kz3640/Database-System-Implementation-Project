8a5a04ac17b856496fb57ee9bea689c510a8afaf
Christian Louria <tian@Christians-MacBook-Air-2.local>
2023-02-08 10:00:08 -0500

    added page split and buffer connection

--- BinaryReader.java
+++ BinaryReader.java
@@ -1,6 +1,5 @@
 import java.io.DataInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
@@ -11,16 +10,18 @@ import Schema.SchemaAttribute;
 import Schema.Varchar;
 
 public class BinaryReader {
+    private Schema schema;
 
-    public BinaryReader() {
-        return;
+    public BinaryReader(Schema schema) {
+        this.schema = schema;
     }
 
-    public Schema getSchema(String filePath) {
+    public Schema getSchema() {
         ArrayList<SchemaAttribute> attributes = new ArrayList<>();
-        try (FileInputStream inputStream = new FileInputStream(filePath)) {
+        try (FileInputStream inputStream = new FileInputStream(this.schema.getPath() + "catalog.txt")) {
             DataInputStream dis = new DataInputStream(inputStream);
 
+            int pageSize = dis.readInt();
             String tableName = dis.readUTF();
             while (dis.available() > 0) {
                 String attrName;
@@ -36,7 +37,7 @@ public class BinaryReader {
                     isPrimary = true;
                     resultChar = dis.readChar();
                 }
-                
+
                 // is string, need length
                 if (resultChar == 'v') {
                     int stringLength = dis.readInt();
@@ -45,35 +46,34 @@ public class BinaryReader {
                     attributes.add(new BICD(attrName, resultChar, isPrimary, false));
                 }
             }
-            return new Schema(tableName, attributes);
+            this.schema.setAttributes(attributes);
+            this.schema.setTableName(tableName);
+            return this.schema;
         } catch (IOException e) {
         }
 
         return null;
     }
 
-    public void readPage(RandomAccessFile raf, ArrayList<SchemaAttribute> schema,
-            ArrayList<ArrayList<Object>> dataList, int bytesToRead) throws IOException {
+    public Page readPage(RandomAccessFile raf, int bytesToRead, int pageId) throws IOException {
         long positionBefore = raf.getFilePointer();
 
+        ArrayList<SchemaAttribute> schemaAttributes = this.schema.getAttributes();
+        ArrayList<ArrayList<Object>> dataList = new ArrayList<ArrayList<Object>>();
+
         while (raf.getFilePointer() - positionBefore != bytesToRead) {
-            // System.out.println("------");
-            // System.out.println(fis.getChannel().position());
-            // System.out.println(positionBefore);
-            // System.out.println(fis.getChannel().position() - positionBefore);
-            // System.out.println(bytesToRead);
             ArrayList<Object> data = new ArrayList<>();
             int dataLength = raf.readInt();
             // needed later
 
-            int numBits = schema.size();
+            int numBits = schemaAttributes.size();
             int numBytes = (int) Math.ceil(numBits / 8.0);
             byte[] nullBitMap = new byte[numBytes];
             raf.read(nullBitMap);
 
             int bitIndex = 0;
             // read null bit map here.
-            for (SchemaAttribute c : schema) {
+            for (SchemaAttribute c : schemaAttributes) {
                 if ((nullBitMap[bitIndex / 8] & (1 << (bitIndex % 8))) != 0) {
                     data.add(null);
                     bitIndex++;
@@ -100,52 +100,42 @@ public class BinaryReader {
             }
             dataList.add(data);
         }
+
+        return new Page(pageId, dataList, this.schema);
     }
 
     // schema needs to be removed from parameters bec page doesnt have access to it
-    public ArrayList<ArrayList<Object>> getPage(String fileName, int pageNumber, Schema schema) throws IOException {
-        ArrayList<ArrayList<Object>> dataList = new ArrayList<>();
-
-        int skipBytes = (pageNumber * 100) + 4;
+    public Page getPage(int pageNumber) throws IOException {
+        String fileName = this.schema.getPath() + "database.txt";
 
         RandomAccessFile raf = new RandomAccessFile(fileName, "rw");
+
+        int totalPages = raf.readInt();
+
+        // 0 pages means no pages
+        // page number 0 means the first page
+        if (totalPages <= pageNumber) {
+            raf.close();
+            System.out.println("No page found at index " + pageNumber);
+            return null;
+        }
+        int pageIndex = pageNumber;
+
+        int skipBytes = (pageIndex * this.schema.getPageSize()) + 4;
         raf.seek(skipBytes);
 
         int pageId = raf.readInt();
         int junkDataSize = raf.readInt();
-        int bytesToRead = 100 - junkDataSize - 8;
-        readPage(raf, schema.getAttributes(), dataList, bytesToRead);
+        int bytesToRead = this.schema.getPageSize() - junkDataSize - 8;
+        Page page = readPage(raf, bytesToRead, pageId);
 
-        return dataList;
+        return page;
     }
 
-    public ArrayList<ArrayList<Object>> getAllRecords(String fileName, ArrayList<SchemaAttribute> schema) throws IOException {
-        ArrayList<ArrayList<Object>> dataList = new ArrayList<>();
-        RandomAccessFile raf = new RandomAccessFile(fileName, "rw");
-
-        if (raf.length() == 0) {
-            raf.close();
-            return dataList;
-        }
-
-        int pagesToRead = raf.readInt();
-        int pagesRead = 0;
-        while (pagesToRead > pagesRead) {
-            int pageId = raf.readInt();
-            int junkDataSize = raf.readInt();
-
-            int bytesToRead = 100 - junkDataSize - 8;
-            readPage(raf, schema, dataList, bytesToRead);
-            raf.skipBytes(junkDataSize);
-            pagesRead++;
-        }
-        return dataList;
-    }
-
-    public ArrayList<Object> readRecord(String fileName, ArrayList<SchemaAttribute> schema) throws IOException {
+    public ArrayList<Object> readRecord(String fileName) throws IOException {
         ArrayList<Object> data = new ArrayList<>();
         try (DataInputStream dis = new DataInputStream(new FileInputStream(fileName))) {
-            for (SchemaAttribute c : schema) {
+            for (SchemaAttribute c : this.schema.getAttributes()) {
                 switch (c.getLetter()) {
                     case 'i':
                         data.add(dis.readInt());
@@ -166,6 +156,22 @@ public class BinaryReader {
             }
         }
         return data;
+    }
+
+    public void printDB() throws IOException {
+        RandomAccessFile raf = new RandomAccessFile(this.schema.getPath() + "database.txt", "rw");
+
+        if (raf.length() == 0) {
+            System.out.println("db not init");
+            raf.close();
+        }
+
+        int pagesToRead = raf.readInt();
+        int pagesRead = 0;
+        while (pagesToRead > pagesRead) {
+            getPage(pagesRead).printPage();
+            pagesRead++;
+        }
     }
 
 }
--- BinaryWriter.java
+++ BinaryWriter.java
@@ -1,36 +1,22 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 import Schema.Schema;
 
 public class BinaryWriter {
+    private Schema schema;
 
-    public BinaryWriter() {
+    public BinaryWriter(Schema schema) {
+        this.schema = schema;
         return;
     }
 
-    // read until end of tile
-    // need to change this to read until correct spot to insert
-    // while (true) {
-    // try {
-    // int prevRecordSize = dis.readInt();
-    // dis.skip(prevRecordSize);
-    // } catch (java.io.EOFException e) {
-    // break;
-    // }
-    // }
-
-    public void writeRecordToFile(ArrayList<Object> data, String fileName, RandomAccessFile raf) throws IOException {
-
-        int recordSize = calculateBytes(data);
+    public void writeRecordToFile(ArrayList<Object> data, RandomAccessFile raf) throws IOException {
+        String fileName = this.schema.getPath() + "database.txt";
+        int recordSize = Util.calculateBytes(data);
         int numBits = data.size();
         int numBytes = (int) Math.ceil(numBits / 8.0);
         byte[] nullBitMap = new byte[numBytes];
@@ -57,90 +43,102 @@ public class BinaryWriter {
         }
     }
 
-    public int calculatePageSize(ArrayList<ArrayList<Object>> allRecords) {
-        int totalSize = 0;
-        for (ArrayList<Object> record : allRecords) {
-            totalSize += calculateRecordSize(record);
-        }
-        totalSize += 4; // int id
-        totalSize += 4; // int junkSize
-        return totalSize;
-    }
-
-    public int calculateRecordSize(ArrayList<Object> data) {
-        return this.calculateBytes(data) + 4;
-    }
-
-    public int calculateJunkSpaceSize(ArrayList<ArrayList<Object>> allRecords, int pageSize) {
-        return pageSize - calculatePageSize(allRecords);
-    }
-
-    public void writePage(ArrayList<ArrayList<Object>> allRecords, String fileName, Schema schema, int pageNumber,
-            int pageSize) throws FileNotFoundException, IOException {
-
-        int segmentSize = 2;
-        int numSegments = (int) Math.ceil(allRecords.size() / (double) segmentSize);
-        ArrayList<ArrayList<ArrayList<Object>>> segments = new ArrayList<>();
-
-        for (int i = 0; i < numSegments; i++) {
-            int startIndex = i * segmentSize;
-            int endIndex = Math.min((i + 1) * segmentSize, allRecords.size());
-            ArrayList<ArrayList<Object>> segment = new ArrayList<>(allRecords.subList(startIndex, endIndex));
-            segments.add(segment);
-        }
-        int skipBytes = (pageNumber * pageSize) + 4;
-
+    public void insertNewPage(Page page) throws IOException {
+        String fileName = this.schema.getPath() + "database.txt";
         RandomAccessFile raf = new RandomAccessFile(fileName, "rw");
+
+        int skipBytes = ((page.getPageID()) * this.schema.getPageSize()) + 4;
+
+        int totalPages = raf.readInt();
+        totalPages++;
+        raf.seek(0);
+        raf.writeInt(totalPages);
+
+        // add at end of file
+        if (skipBytes >= raf.length()) {
+            writePage(page);
+            raf.close();
+            return;
+        }
+
         raf.seek(skipBytes);
 
-        writePageToFile(raf, segments.get(0), fileName, schema);
+        int bytesToRead = (int) (raf.length() - raf.getFilePointer());
+        byte[] buffer = new byte[bytesToRead];
+        raf.read(buffer, 0, bytesToRead);
+        writePage(page);
+        raf.write(buffer);
+        raf.close();
+
+        increaseAllPageIds(page.getPageID());
+        return;
     }
 
-    public void writeAll(ArrayList<ArrayList<Object>> allRecords, String fileName, Schema schema) throws IOException {
-        int segmentSize = 2;
-        int numSegments = (int) Math.ceil(allRecords.size() / (double) segmentSize);
-        ArrayList<ArrayList<ArrayList<Object>>> segments = new ArrayList<>();
+    private void increaseAllPageIds(int pageIndex) throws IOException {
+        String fileName = this.schema.getPath() + "database.txt";
+        RandomAccessFile raf = new RandomAccessFile(fileName, "rw");
+        int totalPages = raf.readInt();
 
-        for (int i = 0; i < numSegments; i++) {
-            int startIndex = i * segmentSize;
-            int endIndex = Math.min((i + 1) * segmentSize, allRecords.size());
-            ArrayList<ArrayList<Object>> segment = new ArrayList<>(allRecords.subList(startIndex, endIndex));
-            segments.add(segment);
+        int pageToChange = pageIndex;
+        while (pageToChange < totalPages) {
+            int skipBytes = (pageToChange * this.schema.getPageSize()) + 4;
+            raf.seek(skipBytes);
+            raf.writeInt(pageToChange);
+            pageToChange++;
         }
+        raf.close();
+    }
+
+    public int getTotalPages() throws IOException {
+        RandomAccessFile raf = new RandomAccessFile(this.schema.getPath() + "database.txt", "rw");
+        int totalPages = raf.readInt();
+        raf.close();
+        return totalPages;
+    }
+
+    public void initDB() throws IOException {
+        File db = new File(this.schema.getPath() + "database.txt");
+        db.createNewFile();
+        RandomAccessFile raf = new RandomAccessFile(this.schema.getPath() + "database.txt", "rw");
+        raf.writeInt(0);
+        raf.close();
+    }
+
+    public void writePage(Page page) throws FileNotFoundException, IOException {
+
+        String fileName = this.schema.getPath() + "database.txt";
+
+        int skipBytes = (page.getPageID() * this.schema.getPageSize()) + 4;
 
         RandomAccessFile raf = new RandomAccessFile(fileName, "rw");
-        try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(fileName, false))) {
-            raf.writeInt(numSegments);
-            for (ArrayList<ArrayList<Object>> segment : segments) {
-                writePageToFile(raf, segment, fileName, schema);
-            }
-        }
-    }
+        int totalPages = raf.readInt();
 
-    public void writePageToFile(RandomAccessFile raf, ArrayList<ArrayList<Object>> allRecords, String fileName,
-            Schema schema)
-            throws IOException {
-        int pageSize = 100;
-        int junkSpace = calculateJunkSpaceSize(allRecords, pageSize);
-        raf.writeInt(Integer.parseInt("1"));
+        // if we are adding a new page then we need to increment the total pages
+        if (page.getPageID() >= totalPages) {
+            totalPages++;
+            raf.seek(0);
+            raf.writeInt(totalPages);
+        }
+
+        raf.seek(skipBytes);
+
+        int junkSpace = Util.calculateJunkSpaceSize(page.getRecords(), this.schema.getPageSize());
+        raf.writeInt(page.getPageID());
         raf.writeInt(junkSpace);
-        for (ArrayList<Object> record : allRecords) {
-            writeRecordToFile(record, fileName, raf);
+        for (ArrayList<Object> record : page.getRecords()) {
+            writeRecordToFile(record, raf);
         }
 
         byte[] junk = new byte[junkSpace];
         raf.write(junk);
     }
 
-    public void writeSchemaToFile(ArrayList<Object> data, String fileName) throws IOException {
-        // try (DataOutputStream dos = new DataOutputStream(new
-        // FileOutputStream(fileName, true));
-        // DataInputStream dis = new DataInputStream(new FileInputStream(fileName))) {
+    public void writeSchemaToFile(ArrayList<Object> data) throws IOException {
+        String fileName = this.schema.getPath() + "catalog.txt";
         RandomAccessFile raf = new RandomAccessFile(fileName, "rw");
         for (Object o : data) {
             writeDataType(o, fileName, raf);
         }
-        // }
     }
 
     public void writeDataType(Object o, String fileName, RandomAccessFile raf) throws IOException {
@@ -155,61 +153,5 @@ public class BinaryWriter {
         } else if (o instanceof Double) {
             raf.writeDouble((Double) o);
         }
-    }
-
-    public int calculateBytes(ArrayList<Object> data) {
-        int size = 0;
-        int numOfBits = data.size();
-        int numOfBytes = (int) Math.ceil((double) numOfBits / 8);
-        size += numOfBytes;
-        for (Object o : data) {
-            if (o == null) {
-                continue;
-            }
-            if (o instanceof Integer) {
-                size += 4;
-            } else if (o instanceof Boolean) {
-                size += 1;
-            } else if (o instanceof Character) {
-                size += 2;
-            } else if (o instanceof String) {
-                size += ((String) o).getBytes().length + 2;
-            } else if (o instanceof Double) {
-                size += 8;
-            }
-        }
-        return size;
-    }
-
-    public ArrayList<Object> addDataToArray(String[] input) {
-        ArrayList<Object> data = new ArrayList<Object>();
-        for (String s : input) {
-            if (s.equals("null")) {
-                data.add(null);
-                continue;
-            }
-            try {
-                int i = Integer.parseInt(s);
-                data.add(i);
-            } catch (NumberFormatException e) {
-                String lowerString = s.toLowerCase();
-                if (lowerString.equals("true") || lowerString.equals("false")) {
-                    boolean b = Boolean.parseBoolean(s);
-                    data.add(b);
-                } else {
-                    try {
-                        double d = Double.parseDouble(s);
-                        data.add(d);
-                    } catch (NumberFormatException exx) {
-                        if (s.length() == 1) {
-                            data.add(s.charAt(0));
-                        } else {
-                            data.add(s);
-                        }
-                    }
-                }
-            }
-        }
-        return data;
     }
 }
