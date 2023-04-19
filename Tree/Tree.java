1ffc6717d0b59fa38f612c320e5f235042a7a770
Dorrell <abd5126@g.rit.edu>
2023-04-19 11:28:39 -0400

    Created tree and treenode classes with basic things Needs insert, delete, and update now

---
+++ Tree/Tree.java
@@ -0,0 +1,25 @@
+package Tree;
+
+public class Tree {
+    
+    private TreeNode rootNode;
+
+    
+    public Tree(int n) {
+        this.rootNode = new TreeNode(null, n);
+    }
+
+    
+    public void insert() {
+
+    }
+
+    public void delete() {
+
+    }
+
+    public void update() {
+
+    }
+
+}
